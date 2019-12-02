3eddb7b6b76e64deba6b2ea6da8a0c12e9eb8a8b
MATHIEU loïc <loic.mathieu@student.hers.be>
2019-12-02 21:06:34 +0100

    Initial commit

---
+++ app/src/main/java/be/hers/info/inscriptionexamens/MainActivity.java
@@ -0,0 +1,16 @@
+package be.hers.info.inscriptionexamens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
