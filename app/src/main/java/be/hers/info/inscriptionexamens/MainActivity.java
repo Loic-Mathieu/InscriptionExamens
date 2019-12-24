815e8349d21738a30dcda149f08765d286641931
MATHIEU loïc <loic.mathieu@student.hers.be>
2019-12-24 01:38:53 +0100

    Page connexion

--- app/src/main/java/be/hers/info/inscriptionexamens/MainActivity.java
+++ app/src/main/java/be/hers/info/inscriptionexamens/MainActivity.java
@@ -2,16 +2,62 @@ package be.hers.info.inscriptionexamens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity
 {
+    private boolean isUser(String matricule, String password)
+    {
+        // TODO : DB check
+        return true;
+    }
+
+    private boolean isTeacher(String matricule)
+    {
+        // TODO : DB check
+        return false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //COUCOU K
+
+
+        // Boutton connexion
+        Button button = findViewById(R.id.bConnect);
+
+        // Identifiants
+        final EditText matricule = findViewById(R.id.iMatricule);
+        final EditText password = findViewById(R.id.iPassword);
+
+        button.setOnClickListener(
+            new View.OnClickListener()
+            {
+                public void onClick(View v)
+                {
+                    if(isUser(matricule.getText().toString(), password.getText().toString()))
+                    {
+                        if(isTeacher(matricule.getText().toString()))
+                        {
+                            Intent intent = new Intent(getApplicationContext(), MainPage_etud.class);
+                            // intent.putExtra("name", val);
+                            startActivity(intent);
+                        }
+                        else
+                        {
+                            Intent intent = new Intent(getApplicationContext(), MainPage_etud.class);
+                            // intent.putExtra("name", val);
+                            startActivity(intent);
+                        }
+                    }
+                }
+            }
+        );
     }
 }
