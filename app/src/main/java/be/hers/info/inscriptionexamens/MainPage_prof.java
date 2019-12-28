3d9c2293e4f5a5b0d29366a0473fd4d4a68a44b6
Ludwig <ludovic.joinnot@student.hers.be>
2019-12-28 11:57:53 +0100

    Add_MainPage_Prof

---
+++ app/src/main/java/be/hers/info/inscriptionexamens/MainPage_prof.java
@@ -0,0 +1,17 @@
+package be.hers.info.inscriptionexamens;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainPage_prof extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+
+    }
+}
